78627e7d264798d87a83d0851754bb5be9d4595b
Davesakraken <davidgeorgebell1995@outlook.com>
2023-09-09 13:15:15 +0100

    dynamic input placeholder

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,8 +1,11 @@
 import { IconArrowSvg } from "@/components/svgs";
 import { useIpSearch } from "@/hooks/ipSearchContext";
+import { useMediaQuery } from "react-responsive";
 
 export default function SearchBar() {
   const { search, setSearch, handleApiCall } = useIpSearch();
+
+  const isMobile = useMediaQuery({ minWidth: 1150 });
 
   const HandleClick = () => {
     if (!search) {
@@ -16,15 +19,15 @@ export default function SearchBar() {
   return (
     <section className="flex h-12 rounded-xl overflow-clip">
       <input
-        className="w-[17rem] p-4 lg:w-[30rem]"
+        className="w-[17rem] p-4 lg:w-[30rem] "
         type="text"
-        placeholder="Search for any IP address or domain"
+        placeholder={isMobile ? "Search for any IP address or domain" : "Search for any IP address"}
         value={search}
         onChange={(e) => {
           setSearch(e.currentTarget.value);
         }}
       />
-      <button onClick={HandleClick} className="w-12 flex justify-center items-center bg-black" type="submit">
+      <button onClick={HandleClick} className="w-12 flex justify-center items-center bg-black hover:bg-slate-700" type="submit">
         <IconArrowSvg />
       </button>
     </section>
