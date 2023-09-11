2771d833d16a662717d4432d62c2451f5c81316e
Davesakraken <davidgeorgebell1995@outlook.com>
2023-09-11 23:50:02 +0100

    attribution animation

--- src/components/Attribution.tsx
+++ src/components/Attribution.tsx
@@ -3,40 +3,53 @@ import { InformationSvg } from "./svgs";
 
 export default function Attribution() {
   const [visable, setVisable] = useState(false);
+  const [animateOut, setAnimateOut] = useState(false);
 
   useEffect(() => {
     const AttributionTimer = setTimeout(() => {
       setVisable(false);
     }, 7000);
-    return () => clearTimeout(AttributionTimer);
+
+    const animationTimerIn = setTimeout(() => {
+      setAnimateOut(true);
+    }, 6000);
+    const animationTimerOut = setTimeout(() => {
+      setAnimateOut(false);
+    }, 7000);
+
+    console.log(`animateOut: ${animateOut}, visable: ${visable}`);
+    return () => {
+      clearTimeout(AttributionTimer), clearTimeout(animationTimerIn), clearTimeout(animationTimerOut);
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [visable]);
 
-  if (!visable) {
-    return (
-      <div className="absolute w-full flex justify-center z-[500] bottom-5 animate-fade-in-fwd">
-        <a
-          onClick={() => {
-            setVisable(true);
-          }}
-        >
-          <InformationSvg />
-        </a>
-      </div>
-    );
-  } else {
-    return (
-      <div className="absolute w-full flex justify-center z-[500] bottom-5 animate-slide-in-bottom">
-        <div className="absolute bottom-0 z-[500] w-[17rem] lg:w-[22rem] p-1 text-sm text-center bg-white rounded-lg shadow-lg">
-          Challenge by{" "}
-          <a target="_blank" href="https://www.frontendmentor.io/profile/Davesakraken">
-            Frontend Mentor.{" "}
-          </a>
-          Coded by{" "}
-          <a target="_blank" href="https://www.davidbell.xyz/">
-            David Bell.
+  return (
+    <div className="absolute w-full flex justify-center z-[500] bottom-5">
+      {!visable ? (
+        <div>
+          <a
+            onClick={() => {
+              setVisable(true);
+            }}
+          >
+            <InformationSvg additionalClassNames="animate-fade-in-fwd" />
           </a>
         </div>
-      </div>
-    );
-  }
+      ) : (
+        <div className={`absolute w-full flex justify-center z-[500] bottom-0 animate-slide-${animateOut ? "out" : "in"}-bottom`}>
+          <div className="absolute bottom-0 z-[500] w-[17rem] lg:w-[22rem] p-1 text-sm text-center bg-white rounded-lg shadow-lg">
+            Challenge by{" "}
+            <a target="_blank" href="https://www.frontendmentor.io/profile/Davesakraken">
+              Frontend Mentor.{" "}
+            </a>
+            Coded by{" "}
+            <a target="_blank" href="https://www.davidbell.xyz/">
+              David Bell.
+            </a>
+          </div>
+        </div>
+      )}
+    </div>
+  );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,8 +13,9 @@ const config: Config = {
         banner: "url(/pattern-bg-desktop.png)",
       },
       animation: {
-        "slide-in-bottom": "slide-in-bottom 1s ease-in-out both",
+        "slide-in-bottom": "slide-in-bottom 1s ease-in-out forwards",
         "fade-in-fwd": "fade-in-fwd 0.6s cubic-bezier(0.390, 0.575, 0.565, 1.000) both",
+        "slide-out-bottom": "slide-out-bottom 1s ease-in-out forwards",
       },
       keyframes: {
         "slide-in-bottom": {
@@ -35,6 +36,16 @@ const config: Config = {
           "100%": {
             transform: "translateZ(0)",
             opacity: "1",
+          },
+        },
+        "slide-out-bottom": {
+          "0%": {
+            transform: "translateY(0)",
+            opacity: "1",
+          },
+          "100%": {
+            transform: "translateY(20px)",
+            opacity: "0",
           },
         },
       },
